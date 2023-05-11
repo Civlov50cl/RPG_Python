8c14fc7d47e9794aba5cc7865abd93fd1f46226d
Civlov50cl <mytestm@proton.me>
2023-05-11 11:00:14 +0200

    first_code

---
+++ src/game.py
@@ -0,0 +1,65 @@
+import pygame
+import pytmx
+import pyscroll
+
+from src.dialog import DialogBox
+from src.player import Player
+from src.map import Map, MapManager
+
+
+class Game:
+
+    def __init__(self):
+        # création fenètre du jeu
+        self.screen = pygame.display.set_mode((1200, 900))
+        pygame.display.set_caption("Nom du jeu")
+
+        # generer le joueur
+        self.player = Player()
+        self.map_manager = MapManager(self.screen, self.player)
+        self.dialog_box = DialogBox()
+
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+
+    def update(self):
+        self.map_manager.update()
+
+    def run(self):
+
+        clock = pygame.time.Clock()
+
+        #boucle du jeu
+        running = True
+
+        while running:
+
+            self.player.save_location()
+            self.handle_input()
+            self.update()
+            self.map_manager.draw()
+            self.dialog_box.render(self.screen)
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.map_manager.check_npc_collisions(self.dialog_box)
+
+            clock.tick(60)
+
+        pygame.quit()
